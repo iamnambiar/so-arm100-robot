7a6e49b21e929499648f56c9aa2c8fbfd20398bf
Neeraj Cherakara <neeraj.cnambiar@gmail.com>
2025-04-24 08:48:42 +0100

    update moveit config

--- soarm100_moveit_config/launch/moveit_rviz.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_moveit_rviz_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("SO_5DOF_ARM100", package_name="soarm100_moveit_config").to_moveit_configs()
-    return generate_moveit_rviz_launch(moveit_config)
